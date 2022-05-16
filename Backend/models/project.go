7168497ee61af2241d871548f0290d50031aa338
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-16 01:45:17 -0600

    fixed project model

--- Backend/models/project.go
+++ Backend/models/project.go
@@ -6,13 +6,13 @@ type Project struct {
 	Id          primitive.ObjectID `json:"id" bson:"_id, omitempty"`
 	Title       string             `json:"title" bson:"title"`
 	Description string             `json:"description" bson:"description, omitempty"`
-	CreatedBy   []string           `json:"created_by" bson:"created_by"`
+	CreatedBy   string             `json:"created_by" bson:"created_by"`
 	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
 }
 
 type CreateProject struct {
 	Title       string             `json:"title" bson:"title"`
 	Description string             `json:"description" bson:"description"`
-	CreatedBy   []string           `json:"created_by" bson:"created_by"`
+	CreatedBy   string             `json:"created_by" bson:"created_by"`
 	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
 }
