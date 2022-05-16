5802dec36d8dbb47d7d7c47d9ef6dc21815b0559
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-16 01:45:41 -0600

    fixed the ticket model and added the createTicket model

--- Backend/models/ticket.go
+++ Backend/models/ticket.go
@@ -10,7 +10,20 @@ type Ticket struct {
 	Priority    string             `json:"priority" bson:"priority"`
 	Tags        []string           `json:"tags" bson:"tags"`
 	CreatedBy   string             `json:"created_by" bson:"created_by"`
-	AssignedTo  string             `json:"assigned_to" bson:"assigned_to"`
+	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
+	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
+	UpdatedOn   primitive.DateTime `json:"updated_on" bson:"updated_on"`
+	ProjectId   primitive.ObjectID `json:"project_id" bson:"project_id"`
+}
+
+type CreateTicket struct {
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	Status      string             `json:"status" bson:"status"`
+	Priority    string             `json:"priority" bson:"priority"`
+	Tags        []string           `json:"tags" bson:"tags"`
+	CreatedBy   string             `json:"created_by" bson:"created_by"`
+	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
 	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
 	UpdatedOn   primitive.DateTime `json:"updated_on" bson:"updated_on"`
 	ProjectId   primitive.ObjectID `json:"project_id" bson:"project_id"`
