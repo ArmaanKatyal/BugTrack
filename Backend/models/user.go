f03d76f181311ba76e1c951c3011d1b997958086
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-16 23:52:27 -0600

    added the Profile structure

--- Backend/models/user.go
+++ Backend/models/user.go
@@ -18,3 +18,12 @@ type CreateUser struct {
 	Email     string             `json:"email" bson:"email"`
 	CreatedOn primitive.DateTime `json:"created_on" bson:"created_on"`
 }
+
+type Profile struct {
+	FirstName       string   `json:"first_name" bson:"first_name"`
+	LastName        string   `json:"last_name" bson:"last_name"`
+	Username        string   `json:"username" bson:"username"`
+	Email           string   `json:"email" bson:"email"`
+	TicketsCreated  []Ticket `json:"tickets_created" bson:"tickets_created"`
+	TicketsAssigned []Ticket `json:"tickets_assigned" bson:"tickets_assigned"`
+}
