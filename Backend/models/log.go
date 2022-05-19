832b7607e3ba7a1e38213c2933154c2144291f5c
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-19 02:31:19 -0600

    updated the log structure

--- Backend/models/log.go
+++ Backend/models/log.go
@@ -3,8 +3,9 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Log struct {
-	Type   string             `json:"type" bson:"type"`
-	Author string             `json:"author" bson:"author"`
-	Date   primitive.DateTime `json:"date" bson:"date"`
-	Table  string             `json:"table" bson:"table"`
+	Type        string             `json:"type" bson:"type"`
+	Author      string             `json:"author" bson:"author"`
+	Date        primitive.DateTime `json:"date" bson:"date"`
+	Description string             `json:"description" bson:"description"`
+	Table       string             `json:"table" bson:"table"`
 }
