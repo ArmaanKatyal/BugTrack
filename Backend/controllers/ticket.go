66c00e865533458dc262bea14d5cf2e3e4de79df
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-19 02:29:58 -0600

    added the controller to get tickets filtered and added logging ability

--- Backend/controllers/ticket.go
+++ Backend/controllers/ticket.go
@@ -9,6 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
+	"time"
 )
 
 // AllTickets writes the JSON response of all tickets
@@ -18,7 +19,7 @@ func AllTickets(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
+	
 	_, err := authenticate(r)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
@@ -122,7 +123,7 @@ func CreateTicket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := authenticate(r)
+	Author, err := authenticate(r)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
@@ -157,6 +158,20 @@ func CreateTicket(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			return
 		}
+		return
+	}
+
+	logColl := client.Database("bugTrack").Collection("logs")
+	log := models.Log{
+		Type:        "Create",
+		Author:      Author,
+		Date:        primitive.NewDateTimeFromTime(time.Now()),
+		Description: Author + " created a ticket with the id: " + result.InsertedID.(primitive.ObjectID).Hex(),
+		Table:       "tickets",
+	}
+	_, err = logColl.InsertOne(context.TODO(), log)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -198,7 +213,7 @@ func UpdateTicket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := authenticate(r)
+	Author, err := authenticate(r)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
@@ -267,6 +282,20 @@ func UpdateTicket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	logColl := client.Database("bugTrack").Collection("logs")
+	log := models.Log{
+		Type:        "Update",
+		Author:      Author,
+		Date:        primitive.NewDateTimeFromTime(time.Now()),
+		Description: Author + " updated a ticket with the id: " + ticketID.Hex(),
+		Table:       "tickets",
+	}
+	_, err = logColl.InsertOne(context.TODO(), log)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	// set the status to 200 OK
 	w.WriteHeader(http.StatusOK)
 	// set the header to application/json
@@ -299,7 +328,7 @@ func DeleteTicket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := authenticate(r)
+	Author, err := authenticate(r)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
@@ -348,6 +377,20 @@ func DeleteTicket(w http.ResponseWriter, r *http.Request) {
 		Status string `json:"status"`
 	}{
 		Status: "success",
+	}
+
+	logColl := client.Database("bugTrack").Collection("logs")
+	log := models.Log{
+		Type:        "Delete",
+		Author:      Author,
+		Date:        primitive.NewDateTimeFromTime(time.Now()),
+		Description: Author + " deleted a ticket with the id: " + ticketID.Hex(),
+		Table:       "tickets",
+	}
+	_, err = logColl.InsertOne(context.TODO(), log)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	// set the status to 200 OK
@@ -442,4 +485,134 @@ func ProjectTickets(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+func FilterTicketsByStatus(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is GET or not
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, err := authenticate(r)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Get the client connection
+	client := config.ClientConnection()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			return
+		}
+	}()
+
+	coll := client.Database("bugTrack").Collection("tickets")
+
+	// Get the query parameters
+	queryParams := r.URL.Query()
+	typeVal := queryParams.Get("type")
+
+	// Check if the type is provided
+	if typeVal == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Check if the type is valid
+	if typeVal != "open" && typeVal != "closed" && typeVal != "in-progress" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// filter to get the tickets with the status provided
+	filter := bson.D{{"status", typeVal}}
+	cursor, err := coll.Find(context.TODO(), filter)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Create a slice of tickets
+	var tickets []models.Ticket
+	// Iterate through the cursor
+	if err = cursor.All(context.TODO(), &tickets); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// set the status to 200 OK
+	w.WriteHeader(http.StatusOK)
+	// set the header to application/json
+	w.Header().Set("Content-Type", "application/json")
+	// Write the JSON response
+	err = json.NewEncoder(w).Encode(tickets)
+	if err != nil {
+		return
+	}
+}
+
+func FilterTicketsByPriority(w http.ResponseWriter, r *http.Request) {
+	// Check if the request method is GET or not
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, err := authenticate(r)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Get the client connection
+	client := config.ClientConnection()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			return
+		}
+	}()
+
+	coll := client.Database("bugTrack").Collection("tickets")
+
+	// Get the query parameters
+	queryParams := r.URL.Query()
+	typeVal := queryParams.Get("type")
+
+	// Check if the type is provided
+	if typeVal == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Check if the type is valid
+	if typeVal != "low" && typeVal != "medium" && typeVal != "high" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// filter to get the tickets with the status provided
+	filter := bson.D{{"priority", typeVal}}
+	cursor, err := coll.Find(context.TODO(), filter)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// Create a slice of tickets
+	var tickets []models.Ticket
+	// Iterate through the cursor
+	if err = cursor.All(context.TODO(), &tickets); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// set the status to 200 OK
+	w.WriteHeader(http.StatusOK)
+	// set the header to application/json
+	w.Header().Set("Content-Type", "application/json")
+	// Write the JSON response
+	err = json.NewEncoder(w).Encode(tickets)
+	if err != nil {
+		return
+	}
 }
