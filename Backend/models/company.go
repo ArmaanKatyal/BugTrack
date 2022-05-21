a91b354c80a1a61c8fcc44cd74837bc3f0686d5c
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-21 02:30:46 -0600

    created structs for Company

--- Backend/models/company.go
+++ Backend/models/company.go
@@ -1,1 +1,14 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Company struct {
+	Id          primitive.ObjectID `json:"id" bson:"_id"`
+	Name        string             `json:"name" bson:"name"`
+	CompanyCode string             `json:"company_code" bson:"company_code"`
+}
+
+type CreateCompany struct {
+	Name        string `json:"name" bson:"name"`
+	CompanyCode string `json:"company_code" bson:"company_code"`
+}
