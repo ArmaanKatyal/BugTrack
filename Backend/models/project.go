5a0ed6e693171e2337650fcccf8a7ae132e7dd5a
ArmaanKatyal <armaankatyal3@gmail.com>
2022-05-29 03:17:29 -0600

    added new PM property

--- Backend/models/project.go
+++ Backend/models/project.go
@@ -4,23 +4,25 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Project struct holds the project data that is stored in the database
 type Project struct {
-	Id          primitive.ObjectID `json:"id" bson:"_id, omitempty"`
-	Title       string             `json:"title" bson:"title"`
-	Description string             `json:"description" bson:"description, omitempty"`
-	CreatedBy   string             `json:"created_by" bson:"created_by"`
-	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
-	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
-	CompanyCode string             `json:"company_code" bson:"company_code"`
+	Id             primitive.ObjectID `json:"id" bson:"_id, omitempty"`
+	Title          string             `json:"title" bson:"title"`
+	Description    string             `json:"description" bson:"description, omitempty"`
+	CreatedBy      string             `json:"created_by" bson:"created_by"`
+	CreatedOn      primitive.DateTime `json:"created_on" bson:"created_on"`
+	AssignedTo     []string           `json:"assigned_to" bson:"assigned_to"`
+	CompanyCode    string             `json:"company_code" bson:"company_code"`
+	ProjectManager string             `json:"project_manager" bson:"project_manager"`
 }
 
 // CreateProject structs holds the project data when creating a new project
 type CreateProject struct {
-	Title       string             `json:"title" bson:"title"`
-	Description string             `json:"description" bson:"description"`
-	CreatedBy   string             `json:"created_by" bson:"created_by"`
-	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
-	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
-	CompanyCode string             `json:"company_code" bson:"company_code"`
+	Title          string             `json:"title" bson:"title"`
+	Description    string             `json:"description" bson:"description"`
+	CreatedBy      string             `json:"created_by" bson:"created_by"`
+	CreatedOn      primitive.DateTime `json:"created_on" bson:"created_on"`
+	AssignedTo     []string           `json:"assigned_to" bson:"assigned_to"`
+	CompanyCode    string             `json:"company_code" bson:"company_code"`
+	ProjectManager string             `json:"project_manager" bson:"project_manager"`
 }
 
 type CreateProject2 struct {
