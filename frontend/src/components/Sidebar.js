40047d2ee99b4011800f7ebae6026187881e3c1d
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-01 01:44:44 -0600

    updated the sidebar, now shows  tabs based on the user role :)

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,38 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaTicketAlt, FaUserCog } from "react-icons/fa";
+import { FaTicketAlt, FaUserCog, FaBug } from "react-icons/fa";
 import { MdSettingsBackupRestore, MdDashboard, MdLogout } from "react-icons/md";
-
-const menuItems = [
-    { id: 1, name: "Dashboard", link: "/dashboard", icon: MdDashboard },
-    { id: 2, name: "Tickets", link: "/tickets", icon: FaTicketAlt },
-    { id: 3, name: "User Management", link: "/user-management", icon: FaUserCog },
-    { id: 4, name: "System Logs", link: "/system-logs", icon: MdSettingsBackupRestore },
-];
+import { useCookies } from "react-cookie";
 
 function Sidebar(props) {
+    const [cookies, setCookies] = useCookies(["token", "role"]);
+
     return (
         <div className="w-60 h-full shadow-2xl bg-white px-1 absolute font-sans-new">
+            <div className="mt-10 flex flex-col items-center justify-center">
+                <FaBug className="mb-3 text-5xl text-green-900 hover:animate-spin" />
+                <p className="text-2xl font-semibold">
+                    Bug
+                    <span className="text-2xl font-semibold text-green-600">Track</span>
+                </p>
+                <p className="text-sm mt-2">
+                    Role : <span className="text-blue-600">{cookies.role}</span>
+                </p>
+            </div>
             <ul className="relative mt-10">
-                {menuItems.map((item, key) => {
-                    return (
-                        <li className="relative" key={key}>
-                            <Link
-                                to={item.link}
-                                className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
-                                data-mdb-ripple="true"
-                                data-mdb-ripple-color="dark"
-                            >
-                                <item.icon className="mr-2 text-lg" />
-                                <span>{item.name}</span>
-                            </Link>
-                        </li>
-                    );
-                })}
+                <li className="relative">
+                    <Link
+                        to="/dashboard"
+                        className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <MdDashboard className="mr-2 text-lg" />
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+                <li className="relative">
+                    <Link
+                        to="/tickets"
+                        className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <FaTicketAlt className="mr-2 text-lg" />
+                        <span>Tickets</span>
+                    </Link>
+                </li>
+                {cookies.role === "admin" && (
+                    <li className="relative">
+                        <Link
+                            to="/user-management"
+                            className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="dark"
+                        >
+                            <FaUserCog className="mr-2 text-lg" />
+                            <span>User Management</span>
+                        </Link>
+                    </li>
+                )}
+                {cookies.role === "admin" && (
+                    <li className="relative">
+                    <Link
+                        to="/system-logs"
+                        className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="dark"
+                    >
+                        <MdSettingsBackupRestore className="mr-2 text-lg" />
+                        <span>System Logs</span>
+                    </Link>
+                </li>
+                )}
             </ul>
             <Link
                 to="/logout"
-                className="flex relative items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-red-600 hover:bg-gray-100 transition duration-300 ease-in-out"
+                className="flex relative items-center text-sm py-4 px-6 h-12 overflow-hidden text-red-700 text-ellipsis whitespace-nowrap rounded hover:bg-gray-100 transition duration-300 ease-in-out"
             >
                 <MdLogout className="mr-2 text-lg" />
                 <span>Logout</span>
