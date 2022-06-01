65c2f0305c39d75811e52b31b1d57c0e00886572
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-01 01:43:52 -0600

    added new struct to accept data from the react website

--- Backend/models/ticket.go
+++ Backend/models/ticket.go
@@ -28,7 +28,7 @@ type CreateTicket struct {
 	Tags        []string           `json:"tags" bson:"tags"`
 	CreatedBy   string             `json:"created_by" bson:"created_by"`
 	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
-	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on"`
+	CreatedOn   primitive.DateTime `json:"created_on" bson:"created_on, omitempty"`
 	UpdatedOn   primitive.DateTime `json:"updated_on" bson:"updated_on"`
 	ProjectId   primitive.ObjectID `json:"project_id" bson:"project_id"`
 	ProjectName string             `json:"project_name" bson:"project_name"`
@@ -43,4 +43,15 @@ type CreateTicket2 struct {
 	ProjectName string             `json:"project_name" bson:"project_name"`
 	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
 	Tags        []string           `json:"tags" bson:"tags"`
+}
+
+type CreateTicket3 struct {
+	Title       string             `json:"title" bson:"title"`
+	Description string             `json:"description" bson:"description"`
+	Priority    string             `json:"priority" bson:"priority"`
+	ProjectId   primitive.ObjectID `json:"project_id" bson:"project_id"`
+	ProjectName string             `json:"project_name" bson:"project_name"`
+	AssignedTo  []string           `json:"assigned_to" bson:"assigned_to"`
+	Tags        []string           `json:"tags" bson:"tags"`
+	Status      string             `json:"status" bson:"status"`
 }
