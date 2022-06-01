87298177b212084f32590320b4320b47886ca278
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-01 01:45:57 -0600

    added new components to display the log data

--- frontend/src/components/pages/SystemLogs.js
+++ frontend/src/components/pages/SystemLogs.js
@@ -1,24 +1,61 @@
-import React from 'react'
-import Sidebar from '../Sidebar'
+import React from "react";
+import LogCard from "../logsComponents/LogCard";
+import Sidebar from "../Sidebar";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
 function SystemLogs() {
+    const [loading, setLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+    const [cookie, setCookie] = useCookies(["token"]);
+
+    const toggleLoading = () => {
+        setLoading(!loading);
+    };
+
     React.useEffect(() => {
-        if (document.cookie.split("=")[1]) {
-            // do nothing
+        if (cookie.token) {
+            try {
+              axios.get("http://localhost:8080/api/v1/logs", {
+                headers: {
+                  "Content-Type": "application/json",
+                  token: cookie.token,
+                },
+              }).then((res) => {
+                toggleLoading();
+                setData(res.data);
+              }
+              );
+            } catch (err) {
+              window.location.href = "/";
+            }
         } else {
-          window.location.href = "/";
+            window.location.href = "/";
         }
-      }, []);
-  return (
-    <>
-        <Sidebar />
-        <div className="flex flex-col bg-blue-500 h-72 pl-60">
-            <div className="flex flex-col h-full mt-12 pl-36">
-                <h1 className="text-3xl text-white">System Logs</h1>
+    }, []);
+    return (
+        <>
+            <Sidebar />
+            <div className="flex flex-col bg-purple-600 h-72 pl-60">
+                {loading && (
+                    <div className="ml-5 mt-5">
+                        <div
+                            className="spinner-border animate-spin text-white inline-block w-10 h-10 border-3 rounded-full self-start"
+                            role="status"
+                        >
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                )}
+                <div className="relative flex flex-col items-center">
+                    <div className="flex flex-col h-full mt-12 ml-36 self-start">
+                        <h1 className="text-4xl text-white font-sans-new">System Logs</h1>
+                    </div>
+                    <LogCard data={data}/>
+                </div>
             </div>
-        </div>
-      </>
-  )
+        </>
+    );
 }
 
-export default SystemLogs
+export default SystemLogs;
