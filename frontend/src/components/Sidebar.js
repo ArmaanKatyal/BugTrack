5689f2f0a2b39d51878ebbc1f3f909829969f403
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-04 23:22:48 -0600

    added system logs for project-manager

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -55,7 +55,7 @@ function Sidebar(props) {
                         </Link>
                     </li>
                 )}
-                {cookies.role === "admin" && (
+                {(cookies.role === "admin" || cookies.role === "project-manager") && (
                     <li className="relative">
                     <Link
                         to="/system-logs"
