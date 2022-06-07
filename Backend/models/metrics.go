f085e1dbd6d390ce19a70918fe6a42f25c428812
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-07 00:17:38 -0600

    added new structs to handle the metrics data

--- Backend/models/metrics.go
+++ Backend/models/metrics.go
@@ -1,1 +1,34 @@
 package models
+
+type TicketsByStatus struct {
+	Open       int64 `json:"open"`
+	InProgress int64 `json:"inprogress"`
+	Resolved   int64 `json:"resolved"`
+}
+
+type TicketsByPriority struct {
+	Low    int64 `json:"low"`
+	Medium int64 `json:"medium"`
+	High   int64 `json:"high"`
+}
+
+type AdminMetrics struct {
+	TotalProjects     int64             `json:"total_projects"`
+	TotalTickets      int64             `json:"total_tickets"`
+	TicketsByStatus   TicketsByStatus   `json:"tickets_by_status"`
+	TicketsByPriority TicketsByPriority `json:"tickets_by_priority"`
+}
+
+type ProjectManagerMetrics struct {
+	TotalProjects     int64             `json:"total_projects"`
+	TotalTickets      int64             `json:"total_tickets"`
+	TicketsByStatus   TicketsByStatus   `json:"tickets_by_status"`
+	TicketsByPriority TicketsByPriority `json:"tickets_by_priority"`
+}
+
+type DeveloperMetrics struct {
+	TotalProjects     int64             `json:"total_projects"`
+	TotalTickets      int64             `json:"total_tickets"`
+	TicketsByStatus   TicketsByStatus   `json:"tickets_by_status"`
+	TicketsByPriority TicketsByPriority `json:"tickets_by_priority"`
+}
