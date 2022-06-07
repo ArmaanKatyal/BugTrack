2791e983f26badfb4652fdf9cb15f6f835d7e197
ArmaanKatyal <armaankatyal3@gmail.com>
2022-06-07 00:18:32 -0600

    fixed the issue where the component loaded before the data was fetched by the api

--- frontend/src/components/pages/Dashboard.js
+++ frontend/src/components/pages/Dashboard.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Sidebar from "../Sidebar";
 import ProjectCard from "../projectComponents/ProjectCard";
 import { useCookies } from "react-cookie";
+import MetricCard from "../metrics/MetricCard";
 
 const apiPath = "http://localhost:8080/api/v1";
 
@@ -11,23 +12,7 @@ function Dashboard() {
     const [loading, setLoading] = React.useState(true);
     // const [userRole, setUserRole] = React.useState("");
     const [cookie, setCookie] = useCookies(["token", "role"]);
-
-    // const getUserRole = async () => {
-    //     try {
-    //         var config = {
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 token: cookie.token,
-    //             },
-    //         };
-    //         await axios.get(apiPath + "/user/role", config).then((res) => {
-    //             setUserRole(res.data.role);
-    //         });
-    //     } catch (err) {
-    //         // do nothing
-    //     }
-    // };
-    
+    const [metrics, setMetrics] = React.useState(null);
 
     const toggleLoading = () => {
         setLoading(!loading);
@@ -49,10 +34,27 @@ function Dashboard() {
         }
     };
 
+    const getMetrics = async () => {
+        try {
+            var config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    token: cookie.token,
+                },
+            };
+            await axios.get(apiPath + "/metrics", config).then((res) => {
+                setMetrics(res.data);
+            });
+        } catch (err) {
+            // do nothing
+        }
+    };
+
     React.useEffect(() => {
         if (cookie.token) {
             // getUserRole();
             dashboard();
+            getMetrics();
         } else {
             window.location.href = "/";
         }
@@ -65,17 +67,20 @@ function Dashboard() {
                     <div className="flex flex-row h-full mt-12 pl-36 self-start gap-10">
                         <h1 className="text-4xl font-sans-new text-white">Dashboard</h1>
                         {loading && (
-                        <div class="">
-                            <div
-                                class="spinner-border animate-spin text-white inline-block w-10 h-10 border-3 rounded-full self-start"
-                                role="status"
-                            >
-                                <span class="visually-hidden">Loading...</span>
+                            <div class="">
+                                <div
+                                    class="spinner-border animate-spin text-white inline-block w-10 h-10 border-3 rounded-full self-start"
+                                    role="status"
+                                >
+                                    <span class="visually-hidden">Loading...</span>
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
                     </div>
-                    <ProjectCard dashData={data} role={cookie.role} />
+                    <div className="flex flex-col w-11/12 h-auto items-center justify-center mt-20">
+                        <ProjectCard dashData={data} role={cookie.role} />
+                        {metrics && <MetricCard metrics={metrics} />}
+                    </div>
                 </div>
             </div>
         </>
